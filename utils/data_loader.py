146cd2a9e7e1ec59c37f91fac869ed5765c0e753
Chirag Rathi <chiragrathi16798@gmail.com>
2025-11-25 17:44:50 -0600

    made function more generalized

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,19 +1,30 @@
-import numpy as np 
+from pathlib import Path
 import pandas as pd
+from typing import Union
 
+PathLike = Union[str, Path]
 
-def load_data_file(filepath):
-	'''Function to load data required for Famiano calculations.
-	Input: the relative file path.
-	Output: A .csv formatted file'''
+def load_data_file(
+    filepath: PathLike,
+    sep: str = r"\s+",
+    header: int | None = 0,
+) -> pd.DataFrame:
+    """
+    Load a whitespace- or delimiter-separated data file into a DataFrame.
 
-	df = pd.read_csv(filepath,sep='\s+',header=0)
+    Parameters
+    ----------
+    filepath : str or Path
+        Path to the data file.
+    sep : str, optional
+        Field separator passed to `pandas.read_csv`. Defaults to r"\\s+".
+    header : int or None, optional
+        Row number to use as column names. Defaults to 0.
 
-	#print(df.head(5))
-
-	return df
-
-
-# hdp_rxn = load_data_file('../data/NACRE_2/2hdp3h.dat')
-
-# print(hdp_rxn.head(10))
+    Returns
+    -------
+    pandas.DataFrame
+        The loaded data.
+    """
+    filepath = Path(filepath)
+    return pd.read_csv(filepath, sep=sep, header=header)
