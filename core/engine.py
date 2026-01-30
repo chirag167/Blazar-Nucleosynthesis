834f2ae1701b41c238c186dd9dd81e4c10a85fa0
Chirag Rathi <chiragrathi16798@gmail.com>
2026-01-30 13:17:16 -0600

    Added functionality to store abundance histories

--- core/engine.py
+++ core/engine.py
@@ -1,8 +1,12 @@
-# engine.py
 class ReactionNetworkEngine:
-    def __init__(self, state, operators):
+    def __init__(self, state, operators, record_history=False):
         self.state = state
         self.operators = operators
+        self.record_history = record_history
+
+        if record_history:
+            self.t_history = []
+            self.Y_history = []
 
     def step(self):
         self.state.reset_derivatives()
@@ -13,6 +17,10 @@ class ReactionNetworkEngine:
         dt = self.state.compute_dt()
         self.state.apply_update(dt)
         self.state.t += dt
+
+        if self.record_history:
+            self.t_history.append(self.state.t)
+            self.Y_history.append(self.state.Y.copy())
 
         return dt
 
